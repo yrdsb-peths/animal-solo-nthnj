c469cab1367c98e075ff6d1f85e340a9707a53be
nthnj <349045377@gapps.yrdsb.ca>
2025-05-07 13:55:30 -0400

    Elephant size and LS RS facing

--- Elephant.java
+++ Elephant.java
@@ -8,19 +8,51 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("eat.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+
+    String facing = "right";
+    
+    public Elephant() {
+        for(int i = 0; i < idleRight.length; i++) {
+            idleRight[i] = new GreenfootImage("images/ezgif-split/tile00" + i + ".png");
+            idleRight[i].scale(100, 100);
+        }
+        
+        for(int i = 0; i < idleRight.length; i++) {
+            idleLeft[i] = new GreenfootImage("images/ezgif-split/tile00" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        
+        setImage(idleRight[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant() {
+        if(facing.equals("right")){
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        } else {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
+    
     public void act()
     {
             if(Greenfoot.isKeyDown("left")) {
-                move(-1);
+                move(-2);
+                facing = "left";
             } else if(Greenfoot.isKeyDown("right")) {
-                move(1);
+                move(2);
+                facing = "right";
             }
             
             eat();
+            
+            animateElephant();
     }
     
     public void eat() {
@@ -29,6 +61,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createPizza();
             world.increaseScore();
+            elephantSound.play();
         }
     }
 }
